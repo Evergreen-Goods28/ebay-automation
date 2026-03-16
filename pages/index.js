babddcd0e5e0ede2fc744f1854b8d2f5d00e160a
Evergreen-Goods28 <evergreengoods8@gmail.com>
2026-03-15 20:30:12 -0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: '40px', fontFamily: 'system-ui' }}>
-      <h1>eBay Automation API</h1>
-      <p>Backend is running!</p>
-      <a href="/api/test" style={{ color: 'blue' }}>Test API →</a>
-    </div>
-  );
-}
